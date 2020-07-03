104675fa507743ac3037bd5d9826e04b0472761a
Gherson David Cruz <66575728+GhersonDC@users.noreply.github.com>
2020-07-02 22:22:04 -0700

    Add files via upload

---
+++ JS 2020/DOM traversing/script.js
@@ -0,0 +1,15 @@
+const parent = document.getElementById('parent');
+
+// console.log(parent.childNodes) //devuelve todos los nodos del parent
+// console.log(parent.children)   //devuelve solo los elementos del parent
+
+// console.log(parent.parentNode) //devulve el tipo de nodo padre del padre
+// console.log(parent.parentElement) //devulve el tipo de nodo padre del padre
+
+// console.log(parent.firstElementChild) //devuelve el primer nodo hijo
+
+// console.log(parent.hasChildNodes()) //devuelve booleano donde devuelve true si contiene un nodo hijo dentro
+
+console.log(parent.nextElementSibling) //localizamos un elemento sibling del parent
+
+
