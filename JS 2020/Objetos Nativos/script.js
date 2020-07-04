b6c62af505959162000ca04edaf3801cdd093045
Gherson David Cruz <66575728+GhersonDC@users.noreply.github.com>
2020-07-03 23:31:33 -0700

    Add files via upload

---
+++ JS 2020/Objetos Nativos/script.js
@@ -0,0 +1,21 @@
+//Nos permite comparar y crear un cuadro de texto simple
+// if(confirm('Deseas continuar?')){
+//     location.href = 'https://reddit.com'
+// }else{
+//     location.reload()
+// }
+
+//------------------------------------
+//imprime los datos del objeto como una tabla
+// const datosTabla = {color:'Blanco',aspecto:'deportivo',categoria:'Deluxe'}
+
+// console.table(datosTabla)
+
+if(addEventListener('click',()=>{
+    window.history.back()
+
+else{
+    console.dir('hola')
+}
+
+
